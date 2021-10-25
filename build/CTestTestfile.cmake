5ed900ddce60c03a35bb7cd44e8452db37476b43
Paradoxxs <aston.dk@gmail.com>
2021-10-25 21:16:30 +0200

    updfate

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/AVHN/git/CTFProjects
+# Build directory: C:/Users/AVHN/git/CTFProjects/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
